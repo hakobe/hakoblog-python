9742fa7e8e3b13ee5c15590bdeb184c917bca9bb
hakobe <hakobe@gmail.com>
2017-01-08 19:57:06 +0900

    add blog model

---
+++ internblog/model/blog.py
@@ -0,0 +1,7 @@
+class Blog():
+    def __init__(self, id, owner_id, title, created, modified):
+        self.id = id
+        self.owner_id = owner_id
+        self.title = title
+        self.created = created
+        self.modified = modified
