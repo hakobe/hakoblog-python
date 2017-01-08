0f5ee09a3d249d671dcf609645dbefea66dd53c7
hakobe <hakobe@gmail.com>
2017-01-08 21:34:00 +0900

    add user model

---
+++ tests/model/test_user.py
@@ -0,0 +1,8 @@
+from internblog.model.user import User
+def test_init():
+    user = User(
+        id = 0,
+        name = 'hakobe932'
+    )
+    assert user.id == 0
+    assert user.name == 'hakobe932'
