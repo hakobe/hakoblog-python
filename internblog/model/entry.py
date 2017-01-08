c83f7c7f3a1ef465e95bedc31beba874b50944f3
hakobe <hakobe@gmail.com>
2017-01-08 21:32:43 +0900

    add entry model

---
+++ internblog/model/entry.py
@@ -0,0 +1,8 @@
+class Entry():
+    def __init__(self, id, blog_id,title, body, created, modified):
+        self.id = id
+        self.blog_id = blog_id
+        self.title = title
+        self.body = body
+        self.created = created
+        self.modified = modified
---
+++ tests/model/test_entry.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from internblog.model.entry import Entry
+
+def test_init():
+    now = datetime.now(),
+    entry = Entry(
+        id = 0,
+        blog_id = 1,
+        title = 'こんにちは',
+        body = '今日は天気が良いですね。',
+        created = now,
+        modified = now,
+    )
+    assert entry.id == 0
+    assert entry.blog_id == 1
+    assert entry.title == 'こんにちは'
+    assert entry.body == '今日は天気が良いですね。'
+    assert entry.created == now
+    assert entry.modified == now
