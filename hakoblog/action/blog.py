8c876121e10e58c9fec92688d913e7d6dd82ffd9
hakobe <hakobe@gmail.com>
2017-01-15 17:04:40 +0900

    Add a method to ensure global blog created

--- hakoblog/action/blog.py
+++ hakoblog/action/blog.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+from hakoblog.action.user import UserAction
+from hakoblog.loader.blog import BlogLoader
+
 
 class BlogAction():
     @classmethod
@@ -18,3 +21,14 @@ class BlogAction():
                 (new_id, owner_id, title, now, now)
             )
         return new_id
+
+    @classmethod
+    def ensure_global_blog_created(cls, db):
+        global_user = UserAction.ensure_global_user_created(db)
+        blog = BlogLoader.find_by_owner_id(db, global_user.id)
+
+        if blog is None:
+            cls.create(db, global_user.id, "%s's blog" % (global_user.name, ))
+            blog = BlogLoader.find_by_owner_id(db, global_user.id)
+
+        return blog
--- tests/action/test_blog.py
+++ tests/action/test_blog.py
@@ -1,12 +1,13 @@
 import tests.hakoblog  # noqa: F401
 
-from nose.tools import eq_
+from nose.tools import eq_, assert_is_none
 
 from hakoblog.db import DB
+from hakoblog.loader.user import UserLoader
 from hakoblog.action.blog import BlogAction
 from hakoblog.loader.blog import BlogLoader
 
-from tests.util import random_string, create_user
+from tests.util import random_string, create_user, global_user
 
 
 def test_create():
@@ -24,3 +25,19 @@ def test_create():
     eq_(found_blog.id, blog_id)
     eq_(found_blog.owner_id, user.id)
     eq_(found_blog.title, title)
+
+
+def test_ensure_global_blog_created():
+    db = DB()
+
+    with global_user(random_string(10)) as global_user_name:
+        assert_is_none(UserLoader.find_by_name(db, global_user_name))
+
+        blog = BlogAction.ensure_global_blog_created(db)
+
+        found_user = UserLoader.find_by_name(db, global_user_name)
+        eq_(blog.owner_id, found_user.id)
+
+        # Check no exeception raises
+        blog_again = BlogAction.ensure_global_blog_created(db)
+        eq_(blog_again.id, blog.id)
