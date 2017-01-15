9254b032299889d2506bfb238f9b4d4c06115be6
hakobe <hakobe@gmail.com>
2017-01-15 16:38:56 +0900

    Add a method to load blog by owner_id

--- hakoblog/loader/blog.py
+++ hakoblog/loader/blog.py
@@ -20,3 +20,22 @@ class BlogLoader():
                 return None
 
         return Blog(**row)
+
+    @classmethod
+    def find_by_owner_id(cls, db, owner_id):
+        with db.cursor() as cursor:
+            cursor.execute(
+                '''
+                SELECT id, owner_id, title
+                FROM blog
+                WHERE owner_id = %s
+                LIMIT 1
+                ''',
+                (owner_id, )
+            )
+            row = cursor.fetchone()
+
+            if row is None:
+                return None
+
+        return Blog(**row)
--- tests/loader/test_blog.py
+++ tests/loader/test_blog.py
@@ -1,6 +1,6 @@
 import tests.hakoblog  # noqa: F401
 
-from nose.tools import eq_
+from nose.tools import eq_, assert_is_none
 
 from hakoblog.db import DB
 from hakoblog.loader.blog import BlogLoader
@@ -8,7 +8,7 @@ from hakoblog.loader.blog import BlogLoader
 from tests.util import create_user, create_blog
 
 
-def test_create():
+def test_find_by_id():
     db = DB()
 
     user = create_user()
@@ -17,3 +17,17 @@ def test_create():
     found_blog = BlogLoader.find_by_id(db, blog.id)
 
     eq_(found_blog.id, blog.id)
+
+    assert_is_none(BlogLoader.find_by_id(db, -1))
+
+
+def test_find_by_owner_id():
+    db = DB()
+
+    blog = create_blog()
+
+    found_blog = BlogLoader.find_by_owner_id(db, blog.owner_id)
+
+    eq_(found_blog.id, blog.id)
+
+    assert_is_none(BlogLoader.find_by_owner_id(db, -1))
